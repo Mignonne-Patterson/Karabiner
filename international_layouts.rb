ee09dba4bdc7c6331aaa8db337750084cf368e75
Mignonne Patterson <Mignonne-Patterson@gmail.com>
2022-01-15 07:05:49 +1200

    Add support for international keyboard layouts and hardware-specific features

--- international_layouts.rb
+++ international_layouts.rb
@@ -1,7 +1,11 @@
-# International Layout Support Module
-def international_layout_support_module()
-  # Define a list of supported keyboard layouts
- layout_options = [
-   { name: 'US', code:'us'}, 
-    {'name': 'UK English','code':'uk' },
-     {
+# International Layout Support Module For Karabiner
+
+This module provides support for international keyboard layouts and hardware-specific features. Users can customize their keyboards based on different regional settings.
+
+def self.apply_international_configurations(device)
+    # Define mappings specific to the German QWERTZ layout
+      if device.layout == 'de'
+        remap_key('y', '\\')          	# Change Y key mapping for umlauts in text input
+	remap_caps_lock_to_esc()     \t  # Use Caps Lock as Esc when needed
+    end
+end
