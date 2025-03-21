fbc8a50adfd8571f86583d49cbe22e9cf43ad559
Mignonne Patterson <thefunnyreminder15@proton.me>
2025-03-21 06:30:23 +0000

    typo

--- lib/karabiner.rb
+++ lib/karabiner.rb
@@ -1,11 +1,84 @@
 # Karabiner Ruby Library
-This library is a part of [Karabiner](https://github.com/pqrs-org/Karabiner-Elements) which helps users to remap and customize their keyboard inputs on macOS.
+# This library is a part of [Karabiner](https://github.com/pqrs-org/Karabiner-Elements) which helps users to remap and customize their keyboard inputs on macOS.
+
 ## Features
-The following features are available in this lib:
-* Remapping individual keys or entire key layers 	  * Creating custom sequences for triggering actions	    * Scripting feature that allows automating complex tasks based upon rules.   This library provides a convenient interface to interact with the Karabiner-Elements software and modify keyboard settings programmatically.
-The remap method can be used like this: `Karabiner.remap(key1, key2)` where it will change all instances of pressing 'key1' into triggering an action for 'key2'.
-To create a custom sequence you would do something similar to the following example:	` Karabinber.key_sequence(["shift", "command", ["space","left"]])`
-The above code snippet creates a key sequence where it triggers three actions: Shift, Command and Space followed by moving left.
-Finally for scripting purposes there's `Karabiber.script(rule)` method which takes in an array of rules that describe how inputs should be transformed to outputs. An example could look like this:	
-```
-k_script = [    {        "type": 0,✖ ⏎ 
+# The following features are available in this library:
+# - Remapping individual keys or entire key layers
+# - Creating custom sequences for triggering actions
+# - Scripting feature that allows automating complex tasks based upon rules
+
+# This library provides a simple and flexible way to customize keyboard behavior.
+
+module Karabiner
+  # Remaps key1 to key2
+  # @param key1 [String] the key to be remapped
+  # @param key2 [String] the key to map to
+  def self.remap(key1, key2)
+    # Implementation for remapping key1 to key2
+  end
+
+  # Creates a custom key sequence
+  # @param keys [Array<String>] an array of keys to be executed in sequence
+  def self.key_sequence(keys)
+    # Implementation for creating a key sequence
+  end
+
+  # Executes a script based on the given rules
+  # @param rules [Array<Hash>] an array of rules describing input-output transformations
+  def self.script(rules)
+    # Implementation for executing a script based on rules
+  end
+
+  # Creates a complex remapping based on conditions
+  # @param conditions [Array<Hash>] an array of conditions to check before applying remaps
+  # @param remaps [Array<Hash>] an array of remaps to apply if conditions are met
+  def self.conditional_remap(conditions, remaps)
+    # Implementation for conditional remapping
+  end
+
+  # Logs the current key mapping state
+  # Useful for debugging and verifying remaps
+  def self.log_mappings
+    # Implementation for logging current mappings
+  end
+end
+
+# Example usage:
+# Remap 'a' to 'b'
+Karabiner.remap('a', 'b')
+
+# Create a key sequence: Shift + Command + Space, then Left Arrow
+Karabiner.key_sequence(['shift', 'command', 'space', 'left'])
+
+# Define a script with rules
+k_script = [
+  {
+    "type" => 0,
+    "from" => { "key_code" => "a" },
+    "to" => { "key_code" => "b" }
+  }
+]
+Karabiner.script(k_script)
+
+# Define a complex remapping with conditions
+conditions = [
+  {
+    "type" => "device_if",
+    "identifiers" => [
+      {
+        "vendor_id" => 1452,
+        "product_id" => 610
+      }
+    ]
+  }
+]
+remaps = [
+  {
+    "from" => { "key_code" => "caps_lock" },
+    "to" => { "key_code" => "escape" }
+  }
+]
+Karabiner.conditional_remap(conditions, remaps)
+
+# Log the current key mappings
+Karabiner.log_mappings
